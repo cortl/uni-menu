6d28cac52afe14ca6c94bd63109fa60a739503da
Cortlan Bainbridge <bainbric@uni.edu>
2016-11-16 13:37:39 -0600

    Removed unused function, calendar functionality should be gucci now.  Added some javascript for formatting and getting the button to work

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -1,34 +1,26 @@
 jQuery(document).ready(function($){	
 	  $('.datepicker').pickadate({
-	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	    // selectMonths: true, // Creates a dropdown to control month
+	    // selectYears: 15, // Creates a dropdown of 15 years to control year
+	    format: 'mmmm d, yyyy',
+		formatSubmit: 'mmmm d, yyyy',
+		hiddenPrefix: undefined,
+		hiddenSuffix: '_submit',
+		hiddenName: undefined,
+		onClose: function() {
+			$("#date_thing").val(picker.get())
+		},
 	  });
-  	// $("#date_thing").datepicker('setDate', new Date());
-  	// $("#calendar_btn").click(function(){
-  	// 	$("#date_thing").datepicker("show");
-  	// });
-  	var $input = $('.datepicker').pickadate();
+	var $input = $('.datepicker').pickadate();
 	var picker = $input.data('pickadate');
   	$("#calendar_btn").click(function(e) {
 	    // stop the click from bubbling
+	    picker.open()
 	    e.stopPropagation();
 	    // prevent the default click action
 	    e.preventDefault();
 	    // open the date picker
-	    $('#date_thing').open
+	    // var what = $('#date_thing').get()
+	    var who = 1
 	});
 });
-function getDate(){
-	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth();
-	var yyyy = today.getFullYear();
-
-	if(dd<10) {
-	    dd='0'+dd
-	} 
-
-	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-	var dayString = months[mm]+" "+dd+","+yyyy;
-	return dayString;
-}
