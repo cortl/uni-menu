67380f6a95d8791dfd0fd82ee1907389437e2d06
Cortlan Bainbridge <bainbric@uni.edu>
2016-11-17 16:29:04 -0600

    food formatting stuff set up, just need to autocomplete some bull and it'll be a good time

---
+++ public/javascript/foods.js
@@ -0,0 +1,89 @@
+omelet = {
+	"serving":"1",
+	"calories":"312",
+	"fatcalories":"4",
+	"fat":"4.8g",
+	"satfat":"0.5g",
+	"transfat":"0g",
+	"sodium":"1025.7mg",
+	"sugar":"0g",
+	"fiber":"1.1g",
+	"protein":"11.4",
+	"carbs":"2.4",
+	"ingredients":"100% air"
+}
+
+homestyle_belgian_waffle = {
+	"serving":"1",
+	"calories":"312",
+	"fatcalories":"4",
+	"fat":"4.8g",
+	"satfat":"0.5g",
+	"transfat":"0g",
+	"sodium":"1025.7mg",
+	"sugar":"0g",
+	"fiber":"1.1g",
+	"protein":"11.4",
+	"carbs":"2.4",
+	"ingredients":"Water, Waffles (Enriched wheat flour bleached (wheat flour, malted barlsy flour, niacin, reduced iron, thiamine mononitrate, riboflavin and folic acid), corn flour, sugar, vegetable oil (soybean and/or cottonseed oil), leavening (sodium monocalcium phosphate), sweet cream powder, salt, malt, and natural flavor. Contains wheat and milk. Manufactured in a facility that processes egg and soy.)"
+}
+
+var authentic_french_toast 
+var sausage_patty
+var breakfast_fried_potatoes
+var honey_wheat_donuts
+var raspberry_chocolate_scone
+var cream_of_asparagus_soup
+var chicken_noodle_soup
+var travis_peppy_chicken_pasta
+var romaine_and_orange_salad
+var lasagna
+var cajun_pork_and_red_beans
+var gluten_free_barbeque_chicken
+var grilled_vegetable_sandwich
+var red_onion_and_shrimp_stir_fry
+var stir_fry_chicken
+var monte_cristo_sandwich
+var spicy_chicken_sandwich
+var garden_burger
+var chicken_sandwich
+var criss_cut_potatoes
+var beef_pizza
+var cheese_pizza
+var supreme_pizza
+var tacos_and_nachos
+var strawberry_lemonade_bar
+var toffee_crunch_cookies
+
+var foods = {
+	"Omelet":omelet,
+	"Homestyle Belgian Waffle":homestyle_belgian_waffle,
+	"Scrambled Eggs":scrambled_eggs,
+	"Authentic French Toast":authentic_french_toast,
+	"Sausage Patty":sausage_patty,
+	"Breakfast Fried Potatoes":breakfast_fried_potatoes,
+	"Honey Wheat Donuts":honey_wheat_donuts,
+	"Raspberry Chocolate Scone":raspberry_chocolate_scone,
+	"Cream of Asparagus Soup":cream_of_asparagus_soup,
+	"Chicken Noodle Soup":chicken_noodle_soup,
+	"Travis' Peppy Chicken Pasta":travis_peppy_chicken_pasta,
+	"Romaine and Orange Salad":romaine_and_orange_salad,
+	"Lasagna":lasagna,
+	"Cajun Pork and Red Beans":cajun_pork_and_red_beans,
+	"Gluten Free Barbeque Chicken":gluten_free_barbeque_chicken,
+	"Grilled Vegetable Sandwich":grilled_vegetable_sandwich,
+	"Red Onion and Shrimp Stir Fry":red_onion_and_shrimp_stir_fry,
+	"Stir Fry Chicken":stir_fry_chicken,
+	"Monte Cristo Sandwich":monte_cristo_sandwich,
+	"Spicy Chicken Sandwich":spicy_chicken_sandwich,
+	"Garden Burger":garden_burger,
+	"Chicken Sandwich":chicken_sandwich,
+	"Criss Cut Potatoes":criss_cut_potatoes,
+	"Beef Pizza":beef_pizza,
+	"Cheese Pizza":cheese_pizza,
+	"Supreme Pizza":supreme_pizza,
+	"Tacos and Nachos":tacos_and_nachos,
+	"Strawberry Lemonade Bar":strawberry_lemonade_bar,
+	"Toffee Crunch Cookies":toffee_crunch_cookies
+
+};
