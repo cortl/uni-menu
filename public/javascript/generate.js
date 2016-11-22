cbdd5e2575d90789641d965cec6e5dd6b3347c6c
Cortlan Bainbridge <bainbric@uni.edu>
2016-11-22 09:39:08 -0600

    Added javascript to generating meal, it just picks three random things and calls it good.

--- public/javascript/generate.js
+++ public/javascript/generate.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
+	var generatedFood = []
 	$("#generate_btn").click(function(){
 		$("#generate-block").show();
 		$("#menu-block").hide();
@@ -8,34 +9,63 @@ jQuery(document).ready(function($){
 		$("#generate-block").hide();
 		$("#menu-block").show();
 		$("#nav-block").show();
-	})
+	});
+	$("#generate_meal_btn").click(function(){
+		// Check if any of the inputs are empty, if they are, send
+		// a toast saying OY FACK OFF M8
 
-	/*function populateMeal() {
-		var table = document.getElementById('mealTable');
+		// Else just populate the meal table with shit.
+		populateMeal()
+	});
+	$("#add_generated_to_meal_btn").click(function(){
+		if(generatedFood.length==0){
+			Materialize.toast('You have to generate food first!', 4000)
+		} else {
+			Materialize.toast('Food has been added to your meal!', 4000)
+		}
+		for (var i = 0; i < generatedFood.length; i++) {
+			addFood(generatedFood[i])
+		}
+	});
+	function generateMealFooter(foodTableList) {
+		var mealTableFooter = ("#generate-table-foot");
+		var calories = 0
+		var protein = 0
+		var fat = 0
+		var carbs = 0
+		for (var i = 0; i < foodTableList.length; i++) {
+			calories += parseFloat(foods[foodTableList[i]].calories)
+			protein += parseFloat(foods[foodTableList[i]].protein)
+			fat += parseFloat(foods[foodTableList[i]].fat)
+			carbs += parseFloat(foods[foodTableList[i]].carbs)
+		}
+		$("#generate_total_calories").text(Math.round(10*calories)/10);
+		$("#generate_total_protein").text(Math.round(10*protein)/10);
+		$("#generate_total_fat").text(Math.round(10*fat)/10);
+		$("#generate_total_carbs").text(Math.round(10*carbs)/10);
+	}
+	function populateMeal() {
+		var mealTable = $("#generate-table");
+		mealTable.empty();
+		// get three things from the food list
+		var foodList = []
+		foodList.push(getRandomArrayElement(totalFoodList));
+		foodList.push(getRandomArrayElement(totalFoodList));
+		foodList.push(getRandomArrayElement(totalFoodList));
 
-		var row0 = table.insertRow(0);
-		var row1 = table.insertRow(1);
-		var row3 = table.insertRow(2);
-
-		var cell00 = row.insertCell(0);
-		var cell01 = row.insertCell(1);
-		var cell02 = row.insertCell(2);
-		var cell03 = row.insertCell(3);
-		var cell04 = row.insertCell(4);
-
-		var cell10 = row.insertCell(0);
-		var cell11 = row.insertCell(1);
-		var cell12 = row.insertCell(2);
-		var cell13 = row.insertCell(3);
-		var cell14 = row.insertCell(4);
-
-		var cell20 = row.insertCell(0);
-		var cell21 = row.insertCell(1);
-		var cell22 = row.insertCell(2);
-		var cell23 = row.insertCell(3);
-		var cell24 = row.insertCell(4);
-
-		cell00.insert("swag")
-	} */
+		for (var i = 0; i < foodList.length; i++) {
+			var calories = foods[foodList[i]].calories
+			var carbs = foods[foodList[i]].carbs
+			var protein = foods[foodList[i]].protein
+			var fat = foods[foodList[i]].fat
+			var dumbHtml = "<tr><td><a href=\"#nutrition\" data-toggle=\"nutrition\" data-target=\"#nutrition\" class=\"food_lnk modal-trigger search\"><span class=\"food_lnk search-name\">"+foodList[i]+"</span></a></td><td>"+calories+"</td><td>"+protein+"</td><td>"+fat+"</td><td>"+carbs+"</td></tr>"
+			mealTable.append(dumbHtml);
+		}
+		generatedFood = foodList;
+		generateMealFooter(foodList);
+		$("#add_generated_to_meal_btn").show();
+		// Re-register modals nstuff
+		$(".modal-trigger").leanModal();
+	}
 	
 })
