8cf56f6b84abb3118ae41c5955f6dd1151ec2434
Jonfrancois <francjaa@uni.edu>
2016-11-27 18:22:42 -0600

    Rialto addition

--- index.js
+++ index.js
@@ -15,9 +15,28 @@ app.get('/', function(request, response) {
 app.get('/piazza', function(request, response) {
   response.render('pages/locations/piazza.ejs');
 });
+app.get('/rialto', function(request, response) {
+  response.render('pages/locations/rialto.ejs');
+});
+app.get('/mauker', function(request, response) {
+  response.render('pages/locations/mauker.ejs');
+});
 app.get('/chats', function(request, response) {
   response.render('pages/locations/chats.ejs');
 });
+app.get('/23rd', function(request, response) {
+  response.render('pages/locations/23rd.ejs');
+});
+app.get('/biscottis', function(request, response) {
+  response.render('pages/locations/biscottis.ejs');
+});
+app.get('/bookbistro', function(request, response) {
+  response.render('pages/locations/bookbistro.ejs');
+});
+app.get('/crunchbreak', function(request, response) {
+  response.render('pages/locations/crunchbreak.ejs');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
